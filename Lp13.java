b1892b33dc9f849dccae3c6c6aa79c5a7023a3d6
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-17 12:49:54 +0530

    Add files via upload

---
+++ Lp13.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Lp13 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=num;i++){
+            if(num%i==0){
+                sum=sum+i;
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
