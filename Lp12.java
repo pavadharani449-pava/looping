ba54ca703469ef0debff882ef1fb963d656e82a5
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-17 11:53:12 +0530

    Add files via upload

---
+++ Lp12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Lp12 {
+    public static void main (String[]args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        for(int i=1;i<=num;i++){
+            if(num%i==0){
+                System.out.println(i);
+
+            }
+        }
+
+    }
+    
+}
